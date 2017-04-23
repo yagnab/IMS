1dd969dee29dbfaa457ffef6654ab183c94ed7ca
Yagna B <yagnab@hotmail.com>
2017-04-23 18:46:08 +0100

    Revert changes need to add initial commit. Nothing important

--- IMS.BL/IMS.BL.Testbed/Program.cs
+++ IMS.BL/IMS.BL.Testbed/Program.cs
@@ -23,7 +23,6 @@ namespace IMS.BL.Testbed
         {
             TestBoundryConditionsOfDescriptionLength();
             NoF5Needed();
-            Console.WriteLine("random");
         }
         public static void TestBoundryConditionsOfDescriptionLength()
         {
